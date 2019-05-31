903b21b33f5efff19fb238976e7cb8eb733a514f
Mikhail Shardin <kriptonit1988@gmail.com>
2019-05-31 16:59:05 +0500

    Updates

---
+++ precache-manifest.ddaa35b74ff6f5fd19621dd03c5ef650.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96186b004e0da972ffab22f80bff88a",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "37a634e4225d97820a70",
+    "url": "/star-db/static/css/main.38233287.chunk.css"
+  },
+  {
+    "revision": "7b2e775d09651a4d781b",
+    "url": "/star-db/static/js/2.763cfa91.chunk.js"
+  },
+  {
+    "revision": "37a634e4225d97820a70",
+    "url": "/star-db/static/js/main.48fcde63.chunk.js"
+  },
+  {
+    "revision": "7fd0c1ce602d36977d9a",
+    "url": "/star-db/static/js/runtime~main.82ef0156.js"
+  }
+]);
